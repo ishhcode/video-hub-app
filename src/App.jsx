61be6255bec6647808a329b75ea97136fb420164
Ishika Aggarwal <ishikaaggarwal003@gmail.com>
2024-04-11 23:35:56 +0530

    basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-// import React from "react";
-// import { Route, Routes } from 'react-router-dom';
-// import Layout from "./Layout";
-// import HomePage from "./pages/HomePage.jsx"
+import React from "react";
+import { Route, Routes } from 'react-router-dom';
+import Layout from "./Layout";
+import HomePage from "./pages/HomePage";
+function App() {
+    return (
+        <> 
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route path="" element={<HomePage />}/>
+                </Route>
+            </Routes>        
+        </>
+    );
+}
 
-// function App() {
-//     return (
-//         <> 
-//             <Routes>
-//                 <Route path="/" element={<Layout />}>
-//                     <Route path="" element={<HomePage />}/>
-//                 </Route>
-//             </Routes>        
-//         </>
-//     );
-// }
-
-// export default App;
+export default App;
---
+++ src/components/Search.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Input from "./Input";
+
+function Search() {
+    return (
+        <>
+            <Input className="bg-[#14181d] rounded-xl px-4" placeholder="Search" />
+        </>
+    );
+}
+
+export default Search;
