7de548e364bfdaf7284c0a09f9841bc1109e6ab5
Ishika Aggarwal <ishikaaggarwal003@gmail.com>
2024-04-11 22:25:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import React from "react";
+// import { Route, Routes } from 'react-router-dom';
+// import Layout from "./Layout";
+// import HomePage from "./pages/HomePage.jsx"
+
+// function App() {
+//     return (
+//         <> 
+//             <Routes>
+//                 <Route path="/" element={<Layout />}>
+//                     <Route path="" element={<HomePage />}/>
+//                 </Route>
+//             </Routes>        
+//         </>
+//     );
+// }
+
+// export default App;
