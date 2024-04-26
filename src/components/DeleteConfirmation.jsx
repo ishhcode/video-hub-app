4cd056c62714c65655fac8785d09394f1a662b0c
Ishika Aggarwal <ishikaaggarwal003@gmail.com>
2024-04-26 19:35:56 +0530

    add a div that centers and make bg-opacity less

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -3,51 +3,80 @@ import { ImBin } from "./icons.js";
 
 function DeleteConfirmation({ onCancel, onDelete,comment, tweet,video }) {
     return (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
-            <div className="text-center sm:p-5 p-3 bg-black border-slate-700 border rounded-xl">
-                <div className="flex justify-center items-start p-3 flex-wrap gap-2 ">
-                    <ImBin
-                        color="red"
-                        size={25}
-                    />
-                    <div className="flex flex-col flex-wrap items-start">
-                        <h1 className="text-bold text-xl mb-1">
-                            Delete
-                            {`${comment ? "Comment" : ""} ${
-                                tweet ? "Tweet" : ""
-                            } ${video ? "Video" : ""}`}{" "}
-                        </h1>
-                        <p className="text-xs text-start text-semibold w-60">
-                            <span>
-                                Are you sure you want to delete this{" "}
-                                {`${comment ? "Comment" : ""} ${
-                                    tweet ? "Tweet" : ""
-                                } ${video ? "Video" : ""}`}
-                                ?
-                            </span>{" "}
-                            <span>
-                                Once its deleted, you will not be able to
-                                recover it.
-                            </span>
-                        </p>
+        <>
+        <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-70 z-50">
+                <form
+                    onSubmit={handleSubmit(updateVideo)}
+                    className=" bg-black space-y-2 border outline-none p-2"
+                >
+                    <div className="flex justify-between items-center border-b border-slate-500 px-3 py-1">
+                        <div>
+                            <h2 className="font-bold">Edit Video</h2>
+                            <p className="text-xs mb-2">
+                                Share where you`ve worked on your profile.
+                            </p>
+                        </div>
+                        <IoCloseCircleOutline
+                            size={23}
+                            onClick={handleClosePopUp}
+                            className="cursor-pointer"
+                        />
                     </div>
-                </div>
-                <div className="font-normal flex gap-2 justify-center">
-                    <button
-                        onClick={onCancel}
-                        className="bg-[#222222] py-1 px-3  rounded-lg sm:text-lg text-sm hover:bg-black cursor-pointer"
-                    >
-                        Cancel
-                    </button>
-                    <button
-                        onClick={onDelete}
-                        className="bg-red-500 py-1 px-3 rounded-lg sm:text-lg text-sm hover:opacity-70 cursor-pointer"
-                    >
-                        Delete
-                    </button>
-                </div>
+                    <div className="p-2 space-y-2">
+                        <Input2
+                            type="file"
+                            label="Thumbnail *"
+                            accept="image/png, image/jpeg"
+                            {...register("thumbnail", {
+                                required: "Thumbnail is required ",
+                            })}
+                        />
+                        <span className="text-red-500 text-xs">
+                            {errors.thumbnail?.message}
+                        </span>
+                        <Input2
+                            type="text"
+                            label="Title *"
+                            // value={title}
+                            {...register("title", {
+                                required: "Title is required",
+                            })}
+                        />
+                        <span className="text-red-500 text-xs">
+                            {errors.title?.message}
+                        </span>
+                        <div className="mb-4">
+                            <label>Description *</label>
+                            <textarea
+                                rows="5"
+                                className="focus:bg-[#222222] text-sm overflow-y-scroll bg-transparent outline-none border w-full mt-1 p-1"
+                                {...register("description", {
+                                    required: "Description is required",
+                                })}
+                            ></textarea>
+                            <span className="text-red-500 text-xs">
+                                {errors.description?.message}
+                            </span>
+                        </div>
+                        <div className="flex gap-3">
+                            <Button
+                                className="flex-1 border p-2"
+                                onClick={handleClosePopUp}
+                            >
+                                Cancel
+                            </Button>
+                            <Button
+                                className="flex-1 bg-purple-500 p-2 font-bold"
+                                textColor="text-black"
+                                type="submit"
+                            >
+                                Update
+                            </Button>
+                        </div>
+                    </div>
+                </form>
             </div>
-        </div>
+    </>
     );
 }
 
